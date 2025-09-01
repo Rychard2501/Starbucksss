d190cd404b3206fe95cebba77f7cff3f34f02c99
Rychard Alves <rychardalves25@gmail.com>
2025-08-31 23:54:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let img= document.querySelector(".grande")
+let borda= document.querySelector(".borda")
+let fundo= document.querySelector(".fundo")
+
+               function trocarImagem(endereco){
+
+img.src= endereco
+
+
+    
+
+}
+
+
+               function mudarCirculo(cor){
+                borda.style.backgroundColor = cor;
+               }
+
+
+
+               function trocarFundo(cor) {
+
+    fundo.style.Color = cor;
+}
+
+
